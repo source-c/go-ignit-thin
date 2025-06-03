95f69686a3a62337ebf95dfdf6d0f6588d4a6b9e
A I <io-devel@copper.mx>
2025-06-03 23:38:05 +0300

    store last example

--- examples/readme/main.go
+++ examples/readme/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	ignite "github.com/source-c/go-ignit-thin"
+	"time"
 )
 
 func example1() (*ignite.Client, error) {
@@ -23,13 +24,13 @@ func example1() (*ignite.Client, error) {
 func example2(client *ignite.Client) {
 	ctx := context.Background()
 	cache, err := client.GetOrCreateCacheWithConfiguration(
-	        ctx, 
-	        ignite.CreateCacheConfiguration(
-	                "test",
-		        ignite.WithCacheAtomicityMode(ignite.AtomicAtomicityMode),
-		        ignite.WithCacheMode(ignite.ReplicatedCacheMode),
-		        ignite.WithReadFromBackup(true),
-	))
+		ctx,
+		ignite.CreateCacheConfiguration(
+			"test",
+			ignite.WithCacheAtomicityMode(ignite.AtomicAtomicityMode),
+			ignite.WithCacheMode(ignite.ReplicatedCacheMode),
+			ignite.WithReadFromBackup(true),
+		))
 	if err != nil {
 		fmt.Printf("Failed to create cache: %s\n", err)
 		return
@@ -50,12 +51,45 @@ func example2(client *ignite.Client) {
 	// Output: Contains key test? true
 }
 
+func example3(client *ignite.Client) {
+	ctx := context.Background()
+	cache, err := client.GetOrCreateCache(ctx, "cache_name")
+	if err != nil {
+		fmt.Printf("Failed to create cache: %s\n", err)
+		return
+	}
+
+	cache = cache.WithExpiryPolicy(1*time.Second, ignite.DurationZero, ignite.DurationZero)
+	err = cache.Put(ctx, "test", "test")
+	if err != nil {
+		fmt.Printf("Failed to put value: %s\n", err)
+		return
+	}
+
+	<-time.After(1200 * time.Millisecond)
+	contains, err := cache.ContainsKey(ctx, "test")
+	if err != nil {
+		fmt.Printf("Failed to invoke contains key operation: %s\n", err)
+		return
+	}
+	fmt.Printf("Contains key %s? %t\n", "test", contains)
+	// Output: Contains key test? false
+}
+
 func main() {
 	client, err := example1()
 	if err != nil {
 		panic(err)
 	}
-	defer client.Close(context.Background())
+
+	defer func(client *ignite.Client, ctx context.Context) {
+		err := client.Close(ctx)
+		if err != nil {
+			fmt.Printf("Failed to close client: %s\n", err)
+		}
+	}(client, context.Background())
 
 	example2(client)
+
+	example3(client)
 }
